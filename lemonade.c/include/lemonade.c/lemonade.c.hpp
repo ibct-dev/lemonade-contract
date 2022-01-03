6753686772148c1c1814eb71f4d9e5229e7d273e
Ubuntu <sweatpotato13@gmail.com>
2022-01-03 12:42:27 +0000

    feat: 🎸 add basic actions

--- lemonade.c/include/lemonade.c/lemonade.c.hpp
+++ lemonade.c/include/lemonade.c/lemonade.c.hpp
@@ -18,47 +18,89 @@ using std::vector;
 typedef uint64_t id_type;
 
 class [[eosio::contract("lemonade.c")]] lemonade : public contract {
-   private:
-    const name feeAccount = "fee.rlc"_n;
-    const name ledgisAccount = "income.rlc"_n;
-    const uint32_t minAuctionPendingTime = 2 * config::secondsPerWeek;
+private:
+  struct [[eosio::table]] product {
+    id_type id;
+    name name;
+    uint32_t
+        duration; // (uint32_t) (eosio::current_time_point().sec_since_epoch());
+    asset current_amount;
+    asset amount_per_account;
+    asset maximum_amount_limit;
+    double minimum_yield;
+    double maximum_yield;
 
-    enum class SettleType { AUCTION, SALE };
-    const vector<pair<string, double>> settleData = {
-        {"auction", 0.05},
-        {"sale", 0.05},
-    };
-    const vector<name> valid_code = {"realcollectn", "led.token"};
+    uint64_t primary_key() const { return id; }
+    uint64_t get_name() const { return name.value; }
+  };
 
-    struct [[eosio::table]] account {
-        asset balance;
+  struct [[eosio::table]] account {
+    id_type id;
+    asset balance;
+    id_type product_id;
+    double current_yield;
+    uint32_t started_at; // (uint32_t)
+                         // (eosio::current_time_point().sec_since_epoch());
+    uint32_t
+        ended_at; // (uint32_t) (eosio::current_time_point().sec_since_epoch());
 
-        uint64_t primary_key() const { return balance.symbol.code().raw(); }
-    };
+    uint64_t primary_key() const { return id; }
+    uint64_t get_product_id() const { return product_id; }
+  };
 
-    typedef eosio::multi_index<"accounts"_n, account> accounts;
-    
-    template <typename T>
-    void cleanTable(name self, uint64_t scope = 0) {
-        uint64_t s = scope ? scope : self.value;
-        T db(self, s);
-        while (db.begin() != db.end()) {
-            auto itr = --db.end();
-            db.erase(itr);
-        }
-    };
+  typedef eosio::multi_index<
+      "products"_n, product,
+      indexed_by<"byname"_n,
+                 const_mem_fun<product, uint64_t, &product::get_name>>>
+      products;
 
-   public:
-    using contract::contract;
-    lemonade(name receiver, name code, datastream<const char *> ds)
-        : contract(receiver, code, ds) {}
+  typedef eosio::multi_index<
+      "accounts"_n, account,
+      indexed_by<"byproductid"_n,
+                 const_mem_fun<account, uint64_t, &account::get_product_id>>>
+      accounts;
 
-    [[eosio::action]] void auction(
-        const name &owner, const NftGuid &nft, const extended_asset &price, 
-        const double &service_fee, const double &ledgis_fee,  
-        const uint32_t &exStartDate, const uint32_t &auStartDate,
-        const uint32_t &auEndDate);
+  void stake(const name &owner, const asset &quantity,
+             const name &product_name);
+  uint32_t now();
+  vector<string> memoParser(const string &memo);
 
-    using auction_action =
-        eosio::action_wrapper<"auction"_n, &lemonade::auction>;
+  template <typename T> void cleanTable(name self, uint64_t scope = 0) {
+    uint64_t s = scope ? scope : self.value;
+    T db(self, s);
+    while (db.begin() != db.end()) {
+      auto itr = --db.end();
+      db.erase(itr);
+    }
+  };
+
+public:
+  using contract::contract;
+  lemonade(name receiver, name code, datastream<const char *> ds)
+      : contract(receiver, code, ds) {}
+
+  [[eosio::action]] void addproduct(
+      const name &product_name, const double &minimum_yield,
+      const double &maximum_yield, const asset &amount_per_account,
+      const optional<asset> &maximum_amount_limit,
+      const optional<uint32_t> &duration);
+
+  [[eosio::action]] void rmproduct(const name &product_name);
+
+  [[eosio::action]] void unstake(const name &owner, const name &product_name);
+
+  [[eosio::action]] void changeyield(
+      const name &owner, const name &product_name, const double &yield);
+
+  [[eosio::on_notify("*::transfer")]] void transfer_event(
+      const name &from, const name &to, const asset &quantity,
+      const string &memo);
+
+  using addproduct_action =
+      eosio::action_wrapper<"addproduct"_n, &lemonade::addproduct>;
+  using rmproduct_action =
+      eosio::action_wrapper<"rmproduct"_n, &lemonade::rmproduct>;
+  using unstake_action = eosio::action_wrapper<"unstake"_n, &lemonade::unstake>;
+  using changeyield_action =
+      eosio::action_wrapper<"changeyield"_n, &lemonade::changeyield>;
 };
--- lemonade.c/src/lemonade.c.cpp
+++ lemonade.c/src/lemonade.c.cpp
@@ -1,5 +1,187 @@
 #include "lemonade.c/lemonade.c.hpp"
 
-void lemonade::auction(const name &owner) {
+void lemonade::addproduct(const name &product_name, const double &minimum_yield,
+                          const double &maximum_yield,
+                          const asset &amount_per_account,
+                          const optional<asset> &maximum_amount_limit,
+                          const optional<uint32_t> &duration) {
+  require_auth(get_self());
 
+  products products_table(get_self(), get_self().value);
+  auto productIdx = products_table.get_index<eosio::name("byname")>();
+  auto existing_product = productIdx.find(product_name.value);
+  check(existing_product == productIdx.end(), "product name already exist");
+
+  check(minimum_yield <= maximum_yield, "maximum yield must be same or bigger "
+                                        "then minimum yield");
+
+  asset zero;
+  zero.amount = 0;
+  zero.symbol = amount_per_account.symbol;
+
+  asset limit = zero;
+  uint32_t dur = 0;
+
+  if (maximum_amount_limit.has_value()) {
+    check(amount_per_account.symbol == maximum_amount_limit->symbol,
+          "must be same amount_per_account and maximum_amount_limit symbol");
+    limit = maximum_amount_limit.value();
+  }
+
+  if (duration.has_value()) {
+    dur = duration.value();
+  }
+
+  products_table.emplace(get_self(), [&](product &a) {
+    a.id = products_table.available_primary_key();
+    a.name = product_name;
+    a.duration = dur;
+    a.current_amount = zero;
+    a.amount_per_account = amount_per_account;
+    a.maximum_amount_limit = limit;
+    a.minimum_yield = minimum_yield;
+    a.maximum_yield = maximum_yield;
+  });
+}
+
+void lemonade::rmproduct(const name &product_name) {
+  require_auth(get_self());
+
+  products products_table(get_self(), get_self().value);
+  auto productIdx = products_table.get_index<eosio::name("byname")>();
+  auto existing_product =
+      productIdx.require_find(product_name.value, "product does not exists");
+
+  productIdx.erase(existing_product);
+}
+
+void lemonade::stake(const name &owner, const asset &quantity,
+                     const name &product_name) {
+  require_auth(owner);
+
+  check(quantity.is_valid(), "invalid quantity");
+  check(quantity.amount > 0, "must transfer positive quantity");
+
+  products products_table(get_self(), get_self().value);
+  auto productIdx = products_table.get_index<eosio::name("byname")>();
+  auto existing_product = productIdx.find(product_name.value);
+  check(existing_product != productIdx.end(), "product does not exist");
+
+  accounts accounts_table(owner, owner.value);
+  auto accountIdx = accounts_table.get_index<eosio::name("byproductid")>();
+  auto existing_account = accountIdx.find(existing_product->id);
+  check(existing_account == accountIdx.end(), "already has same product");
+
+  if (existing_product->amount_per_account.amount != 0) {
+    check(existing_product->amount_per_account.amount >= quantity.amount,
+          "exceed amount per account limits");
+  }
+
+  if (existing_product->maximum_amount_limit.amount != 0) {
+    check(existing_product->maximum_amount_limit.amount >=
+              (existing_product->current_amount.amount + quantity.amount),
+          "exceed product total amount limits");
+  }
+
+  uint32_t started_at = now();
+  uint32_t ended_at = 0;
+  if (existing_product->duration != 0) {
+    ended_at = now() + existing_product->duration;
+  }
+
+  accounts_table.emplace(owner, [&](account &a) {
+    a.id = accounts_table.available_primary_key();
+    a.balance = quantity;
+    a.product_id = existing_product->id;
+    a.current_yield = existing_product->minimum_yield;
+    a.started_at = started_at;
+    a.ended_at = ended_at;
+  });
+
+  productIdx.modify(existing_product, same_payer,
+                    [&](product &a) { a.current_amount += quantity; });
+}
+
+void lemonade::unstake(const name &owner, const name &product_name) {
+  require_auth(owner);
+
+  products products_table(get_self(), get_self().value);
+  auto productIdx = products_table.get_index<eosio::name("byname")>();
+  auto existing_product = productIdx.find(product_name.value);
+  check(existing_product != productIdx.end(), "product does not exist");
+
+  accounts accounts_table(owner, owner.value);
+  auto accountIdx = accounts_table.get_index<eosio::name("byproductid")>();
+  auto existing_account = accountIdx.find(existing_product->id);
+  check(existing_account != accountIdx.end(), "owner does not has product");
+
+  if (existing_account->ended_at != 0) {
+    check(existing_account->ended_at <= now(), "account end time is not over");
+  }
+
+  asset toUnstake = asset(existing_account->balance.amount *
+                              log(existing_account->current_yield),
+                          existing_product->amount_per_account.symbol);
+
+  action(permission_level{get_self(), "active"_n}, "led.token"_n, "transfer"_n,
+         make_tuple(get_self(), owner, toUnstake, string("unstake")))
+      .send();
+
+  productIdx.modify(existing_product, same_payer, [&](product &a) {
+    a.current_amount -= existing_account->balance;
+  });
+
+  accountIdx.erase(existing_account);
+}
+
+void lemonade::changeyield(const name &owner, const name &product_name,
+                           const double &yield) {
+  require_auth(get_self());
+
+  products products_table(get_self(), get_self().value);
+  auto productIdx = products_table.get_index<eosio::name("byname")>();
+  auto existing_product = productIdx.find(product_name.value);
+  check(existing_product != productIdx.end(), "product does not exist");
+
+  accounts accounts_table(owner, owner.value);
+  auto accountIdx = accounts_table.get_index<eosio::name("byproductid")>();
+  auto existing_account = accountIdx.find(existing_product->id);
+  check(existing_account != accountIdx.end(), "owner does not has product");
+
+  accountIdx.modify(existing_account, same_payer,
+                    [&](account &a) { a.current_yield = yield; });
+}
+
+uint32_t lemonade::now() {
+  return (uint32_t)(eosio::current_time_point().sec_since_epoch());
+}
+
+void lemonade::transfer_event(const name &from, const name &to,
+                              const asset &quantity, const string &memo) {
+  if (to != get_self())
+    return;
+
+  vector<string> event = memoParser(memo);
+
+  stake(from, quantity, name(event[0]));
+}
+
+vector<string> lemonade::memoParser(const string &memo) {
+  vector<string> result;
+
+  size_t prev = memo.find(':');
+  result.push_back(memo.substr(0, prev));
+  size_t pos = prev;
+  while (true) {
+    pos = memo.find(':', pos + 1);
+    if (pos == std::string::npos) {
+      result.push_back(memo.substr(prev + 1));
+      break;
+    } else {
+      result.push_back(memo.substr(prev + 1, (pos - (prev + 1))));
+    }
+    prev = pos;
+  }
+
+  return result;
 }
